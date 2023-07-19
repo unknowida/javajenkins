5083e03a4f0b409b5eb19bb84bfe2ee8fd5c2efd
csdev <csa.devomax@gmail.com>
2023-07-19 15:12:12 +0200

    first commit

---
+++ javajenkins.java
@@ -0,0 +1,11 @@
+public class javajenkins {
+    public static void main(String[] args) {
+        System.out.println("Bonjour, World Devops !");
+        Additionner(1, 2);
+    }
+
+    public static int Additionner(int a, int b) {
+        return (a + b);
+    }
+}
+
